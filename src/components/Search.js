b3e8e29fbf76008d97cc29d7b04263f3757636fb
lukabelani <luka.belani@gmail.com>
2020-09-25 00:13:25 +0200

    make condition to only search with a term

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,24 +6,34 @@ const Search = () => {
   const [results, setResults] = useState([]);
 
   useEffect(() => {
-    (async () => {
-      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
-        params: {
-          action: "query",
-          list: "search",
-          origin: "*",
-          format: "json",
-          srsearch: term,
-        },
-      });
+    if (term) {
+      (async () => {
+        const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+          params: {
+            action: "query",
+            list: "search",
+            origin: "*",
+            format: "json",
+            srsearch: term,
+          },
+        });
 
-      setResults(data.query.search);
-    })();
+        setResults(data.query.search);
+      })();
+    }
   }, [term]);
 
   const renderedResults = results.map((result) => {
     return (
       <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            className="ui button"
+          >
+            Go
+          </a>
+        </div>
         <div className="content">
           <div className="header">{result.title}</div>
           <span dangerouslySetInnerHTML={{ __html: result.snippet }} />
